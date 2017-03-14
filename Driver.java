ad6a09ca1c340825f8823c05d5e4dd030c8813be
Kevin Phan <kblphan@gmail.com>
2017-03-14 12:44:33 -0500

    update element

--- Driver.java
+++ Driver.java
@@ -16,13 +16,112 @@ class Database {
 }
 
 class Element implements Comparable<Element> {
-    private String mode;
-    private String mediaType;
+    private String type;
+    private int lastViewed;
+    private int dateStarted;
+    private int dateAdded;
+    private boolean finished;
+    private boolean dropped;
     public Element() {
-    	mode = "";
-    	mediaType = "";
+
     }
     public int compareTo(Element x) {
         return 0;
     }
+
+	/**
+	* Returns value of type
+	* @return
+	*/
+	public String getType() {
+		return type;
+	}
+
+	/**
+	* Sets new value of type
+	* @param
+	*/
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	* Returns value of lastViewed
+	* @return
+	*/
+	public int getLastViewed() {
+		return lastViewed;
+	}
+
+	/**
+	* Sets new value of lastViewed
+	* @param
+	*/
+	public void setLastViewed(int lastViewed) {
+		this.lastViewed = lastViewed;
+	}
+
+	/**
+	* Returns value of dateStarted
+	* @return
+	*/
+	public int getDateStarted() {
+		return dateStarted;
+	}
+
+	/**
+	* Sets new value of dateStarted
+	* @param
+	*/
+	public void setDateStarted(int dateStarted) {
+		this.dateStarted = dateStarted;
+	}
+
+	/**
+	* Returns value of dateAdded
+	* @return
+	*/
+	public int getDateAdded() {
+		return dateAdded;
+	}
+
+	/**
+	* Sets new value of dateAdded
+	* @param
+	*/
+	public void setDateAdded(int dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+
+	/**
+	* Returns value of finished
+	* @return
+	*/
+	public boolean isFinished() {
+		return finished;
+	}
+
+	/**
+	* Sets new value of finished
+	* @param
+	*/
+	public void setFinished(boolean finished) {
+		this.finished = finished;
+	}
+
+	/**
+	* Returns value of dropped
+	* @return
+	*/
+	public boolean isDropped() {
+		return dropped;
+	}
+
+	/**
+	* Sets new value of dropped
+	* @param
+	*/
+	public void setDropped(boolean dropped) {
+		this.dropped = dropped;
+	}
 }
