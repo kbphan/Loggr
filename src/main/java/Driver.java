f3e2e5e976f327b52f0841516991a165c5c2062c
PierceGriffiths <gripwolf@gmail.com>
2017-04-20 13:52:42 -0500

    Added omdbGet() method

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,15 +1,36 @@
 //package loggr;
 import java.util.*;
+import java.net.*;
+import java.io.*;
 // import java.util.ArrayList;
 // import java.util.Comparator;
 // import java.util.List;
 // import java.util.Collections;
 
 public class Driver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        System.out.println(omdbGet("Salt"));
         CLI ui = new CLI();
         ui.run();
     }
+    
+    public static String omdbGet(String title) throws MalformedURLException, IOException
+    {
+        StringBuilder result = new StringBuilder();
+        title = title.replaceAll(" ", "_");
+        URL url = new URL("http://www.omdbapi.com/?t=" + title);
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while((line = rd.readLine()) != null)
+        {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+    
 }
 
 class Database {
