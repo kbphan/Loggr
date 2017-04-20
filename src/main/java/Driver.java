445c9d105aa4cf323340d5e85a7bca00d274f8f2
Kevin Phan <kblphan@gmail.com>
2017-04-20 14:09:16 -0500

    update imdb

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,36 +1,16 @@
-//package loggr;
 import java.util.*;
 import java.net.*;
 import java.io.*;
-// import java.util.ArrayList;
-// import java.util.Comparator;
-// import java.util.List;
-// import java.util.Collections;
+
 
 public class Driver {
     public static void main(String[] args) throws MalformedURLException, IOException {
-        System.out.println(omdbGet("Salt"));
+        LoggrGUI gui = new LoggrGUI();
+        gui.launch();
         CLI ui = new CLI();
         ui.run();
     }
-    
-    public static String omdbGet(String title) throws MalformedURLException, IOException
-    {
-        StringBuilder result = new StringBuilder();
-        title = title.replaceAll(" ", "_");
-        URL url = new URL("http://www.omdbapi.com/?t=" + title);
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-        conn.setRequestMethod("GET");
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        while((line = rd.readLine()) != null)
-        {
-            result.append(line);
-        }
-        rd.close();
-        return result.toString();
-    }
-    
+
 }
 
 class Database {
--- src/main/java/ImdbAPI.java
+++ src/main/java/ImdbAPI.java
@@ -1,8 +1,21 @@
-import java.io.IOException;
-//import okhttp3.OkHttpClient;
-//import okhttp3.Request;
-//import okhttp3.Response;
+import java.io.*;
+import java.net.*;
 
 public class ImdbAPI {
-
+    public static String omdbGet(String title) throws MalformedURLException, IOException
+    {
+        StringBuilder result = new StringBuilder();
+        title = title.replaceAll(" ", "_");
+        URL url = new URL("http://www.omdbapi.com/?t=" + title);
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while((line = rd.readLine()) != null)
+        {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
 }
